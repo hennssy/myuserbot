e1ab48f0aa917d46a9e9b776257e9dfff277e178
hennssy <100162290+hennssy@users.noreply.github.com>
2023-01-26 01:13:10 +0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,26 @@
+import config
+import requests
+
+def pluralForm(amount, variants):
+  amount = abs(amount)
+
+  if amount % 10 == 1 and amount % 100 != 11:
+      variant = 0
+  elif 2 <= amount % 10 <= 4 and (amount % 100 < 10 or amount % 100 >= 20):
+    variant = 1
+  else:
+    variant = 2
+
+  return f"{amount} {variants[variant]}"
+
+def get_flags(flag):
+  flags = []
+
+  for number in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 65536]:
+    if flag & number:
+      flags.append(number)
+
+  return flags
+
+def edit_message(args):
+  requests.post(f'{config.api_url}/messages.edit?access_token={config.token}&peer_id={args[0]}&message={args[2]}&message_id={args[1]}&keep_forward_messages=1&v=5.131')
