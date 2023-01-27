1fe80cb799abd0b4a732236fde81528da64b54d5
hennssy <100162290+hennssy@users.noreply.github.com>
2023-01-28 00:25:33 +0500

    changed logic to giving access

--- main.py
+++ main.py
@@ -1,14 +1,85 @@
 import requests
-import json
 import keep_alive
 import config
 import functions
+from threading import Thread
 from commands import *
 from replit import db
 
+def main(updates):
+    for update in updates:
+      code = update[0]
+        
+      if code == 4:
+        print(update)
+        flags = functions.get_flags(update[2])
+        message_text = update[5]
+        message_id = update[1]
+        from_id = None
+        peer_id = None
+        
+        try:
+          if message_text.split()[0] != '.к':
+            continue
+        except:
+          pass
+          
+        try:
+          peer_id = int(update[3])
+          
+          if peer_id > 2000000000:
+            from_id = int(update[6]['from'])
+            
+            if str(from_id) not in db['allowed'] and from_id != 480656577:
+              continue
+          else:
+            if 2 not in flags:
+              from_id = peer_id
+            else:
+              from_id = 480656577
+        except:
+          pass
+
+        if from_id != None:
+          try:
+            command = message_text.split()[1]
+          except:
+            continue
+
+          if command == '+доступ':
+            if from_id == 480656577:
+              try:
+                id = message_text.split()[2]
+              except:
+                functions.edit_message([peer_id, message_id, 'Введите ID!'])
+                continue
+
+              Access.cmd(id, '+', [peer_id, message_id])
+
+          elif command == '-доступ':
+            if from_id == 480656577:
+              try:
+                id = message_text.split()[2]
+              except:
+                functions.edit_message([peer_id, message_id, 'Введите ID!'])
+                continue
+
+              Access.cmd(id, '-', [peer_id, message_id])
+                
+          elif command == 'ку':
+            if from_id == 480656577:
+              functions.edit_message([peer_id, message_id, 'привет, зайка'])
+            else:
+              functions.send_message([peer_id, 'привет, зайка'])
+
+          elif command == 'инфо':
+            Info.get_info(update, [peer_id, message_id, message_text, from_id])
+
+          elif command == 'стики':
+            Stickers.get_sticks(update, [peer_id, message_id, message_text, from_id])
+
 while True:
   res = requests.post(f'{config.url}?act=a_check&key={db["key"]}&ts={db["ts"]}&wait=25&mode=2&version=3').json()
-  print(res)
 
   if 'failed' in res:
     error_code = res['failed']
@@ -25,38 +96,14 @@ while True:
     updates = res['updates']
   except:
   	continue
-    
-  for update in updates:
-    print(update)
-    code = update[0]
-      
-    if code == 4:
-      flags = functions.get_flags(update[2])
-	                  
-      if 2 in flags:
-        message_text = update[5]
-
-        try:
-          if message_text.split()[0] != '.к':
-            break
-        except:
-          pass
-
-        try:
-          command = message_text.split()[1]
-          message_id = update[1]
-          peer_id = update[3]
-        except:
-          continue
-        
-        if command == 'ку':
-          functions.edit_message([peer_id, message_id, 'привет, зайка'])
-
-        if command == 'инфо':
-          Info.get_info(update, [peer_id, message_id, message_text])
-
-        if command == 'стики':
-          Stickers.get_sticks(update, [peer_id, message_id, message_text])
+  
+  if len(updates) == 0:
+    db['ts'] = res['ts']
+    keep_alive.keep_alive()
+    continue
+  
+  multiprocess = Thread(target = main, args = (updates, ))
+  multiprocess.start()
           
   db['ts'] = res['ts']
   keep_alive.keep_alive()
